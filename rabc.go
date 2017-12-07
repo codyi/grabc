5434b40ae9a456a63c1c1ab114eb75fecde6edd8
李国松 <mail@liguosong.com>
2017-12-07 12:31:08 +0800

    增加注册用户身份、注册用户模型、过滤路由的方法

--- rabc.go
+++ rabc.go
@@ -1,7 +1,6 @@
 package grabc
 
 import (
-	"fmt"
 	"github.com/astaxie/beego"
 	"grabc/controllers"
 	"grabc/libs"
@@ -14,13 +13,21 @@ type IUserIdentify interface {
 }
 
 //用于检测需要权限检查的的路由
-var RegisterControllers []beego.ControllerInterface
+var registerControllers []beego.ControllerInterface
 
 //用于保存外部注册的登录用户实例
-var Identify IUserIdentify
+var identify IUserIdentify
 
 //用于保存外部注册的用户模型实例
-var UserModel models.IUserModel
+var userModel models.IUserModel
+
+//忽律检查的网址
+type ignoreRoute struct {
+	Controller string
+	Route      string
+}
+
+var ignoreRoutes []ignoreRoute
 
 //init function
 func init() {
@@ -31,19 +38,48 @@ func init() {
 	beego.AutoRouter(&controllers.AssignmentController{})
 
 	RegisterController(&controllers.RouteController{}, &controllers.RoleController{}, &controllers.PermissionController{}, &controllers.AssignmentController{})
-	libs.RegisterControllers = &RegisterControllers
-	models.UserModel = &UserModel
+	libs.RegisterControllers = &registerControllers
+	models.UserModel = &userModel
+	ignoreRoutes = make([]ignoreRoute, 0)
 }
 
 //注册需要检查的routes
 func RegisterController(controllers ...beego.ControllerInterface) {
 	for _, controller := range controllers {
-		RegisterControllers = append(RegisterControllers, controller)
+		registerControllers = append(registerControllers, controller)
 	}
 }
 
+//注册登录用户
+func RegisterIdentify(i IUserIdentify) {
+	identify = i
+}
+
+//注册用户的模型表，用于获取用户的id和用户名称
+func RegisterUserModel(m models.IUserModel) {
+	userModel = m
+}
+
+//增加忽律检查的地址，例如site/login,这个就不需要检查权限
+func AppendIgnoreRoute(c, r string) {
+	i := ignoreRoute{}
+	i.Controller = c
+	i.Route = r
+	ignoreRoutes = append(ignoreRoutes, i)
+}
+
 //权限检查
-func CheckAccess() bool {
-	fmt.Println(123456)
+func CheckAccess(controllerName, routeName string) bool {
+	//先检查是否在忽律的路由中
+	if len(ignoreRoutes) > 0 {
+		// for _, ignoreRoute := range ignoreRoutes {
+		// 	return true
+		// }
+	}
+
+	if identify.GetId() <= 0 {
+		return false
+	}
+
 	return false
 }
